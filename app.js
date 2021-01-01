681d99ba5aa4ededf22e37d3deee430104b9153a
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-01-01 12:30:22 +0530

    Setup ejs and add list.ejs

--- app.js
+++ app.js
@@ -3,9 +3,48 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("Working!");
+    const today = new Date();
+    const currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+
+        case 1:
+            day = "Monday";
+            break;
+        
+        case 2:
+            day = "Tuesday";
+            break;
+
+        case 3:
+            day = "Wednesday";
+            break;
+
+        case 4:
+            day = "Thursday";
+            break;
+
+        case 5:
+            day = "Friday";
+            break;
+
+        case 6:
+            day = "Saturday";
+            break;
+        
+        default:
+            console.log(currentDay);
+            break;
+    }
+
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, () => {
