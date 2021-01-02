d8df945ad2d00ec0acc6f27f2bcc5563aeb3517b
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-01-02 19:52:31 +0530

    Complete New list item add

--- app.js
+++ app.js
@@ -5,46 +5,26 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var items = [];
+
 app.get("/", (req, res) => {
     const today = new Date();
-    const currentDay = today.getDay();
-    var day = "";
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "short",
+        year: "numeric"
+    };
+    
+    const day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindOfDay: day, newListItem: items});
+});
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
+app.post("/", (req, res) => {
+    var item = req.body.newItem;
+    items.push(item);
 
-        case 1:
-            day = "Monday";
-            break;
-        
-        case 2:
-            day = "Tuesday";
-            break;
-
-        case 3:
-            day = "Wednesday";
-            break;
-
-        case 4:
-            day = "Thursday";
-            break;
-
-        case 5:
-            day = "Friday";
-            break;
-
-        case 6:
-            day = "Saturday";
-            break;
-        
-        default:
-            console.log(currentDay);
-            break;
-    }
-
-    res.render("list", {kindOfDay: day});
+    res.redirect("/");
 });
 
 app.listen(3000, () => {
