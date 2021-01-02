cbe337caf0b574f7f38beaf5c4399bea786de642
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-01-02 21:03:48 +0530

    Add styles

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 let items = [];
