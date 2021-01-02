752cd45d9e25bc8af724806f89a3d5ab545c0d68
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-01-03 00:27:38 +0530

    Declare dynamic port

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+const port = process.env.PORT || 3000;
+
 let items = [];
 
 app.get("/", (req, res) => {
