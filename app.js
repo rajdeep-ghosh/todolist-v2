7e69ec33583701e7d0bf535f15e2400de6ed24e1
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-01-03 20:49:00 +0530

    Add about.ejs file

--- app.js
+++ app.js
@@ -39,6 +39,10 @@ app.get("/work", (req, res) => {
     res.render("list", {listTitle: "Work", newListItem: workItems});
 });
 
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
 app.listen(port, () => {
     console.log("Server started");
 });
