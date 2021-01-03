d449668e5e8f188b6a4fd1611472360060a1f993
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-01-03 20:31:16 +0530

    Add work list route and functionality

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("view engine", "ejs");
 const port = process.env.PORT || 3000;
 
 let items = [];
+let workItems = [];
 
 app.get("/", (req, res) => {
     const today = new Date();
@@ -20,16 +21,24 @@ app.get("/", (req, res) => {
     };
     
     const day = today.toLocaleDateString("en-US", options);
-    res.render("list", {kindOfDay: day, newListItem: items});
+    res.render("list", {listTitle: day, newListItem: items});
 });
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
-    items.push(item);
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
 
-    res.redirect("/");
+app.get("/work", (req, res) => {
+    res.render("list", {listTitle: "Work", newListItem: workItems});
 });
 
 app.listen(port, () => {
-    console.log("Server started at port 3000");
+    console.log("Server started");
 });
